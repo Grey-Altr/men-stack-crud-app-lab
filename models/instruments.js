5997034f90fcb817669224bfd740c3dded4b92c5
GreyA <grey.altaer@gmail.com>
2025-03-04 11:51:49 -0800

    instrumentSchema created: models/instruments.js; doc import: server.js

---
+++ models/instruments.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const instrumentSchema = new mongoose.Schema({
+    name: String,
+    manufacturer: String,
+    release: Date,
+    owned: Boolean,
+});
+
+const Instrument = mongoose.model('Instrument', instrumentSchema);
+
+module.export = Instrument;
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-})
+});
+const Instrument = require('./models/instruments.js');
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
