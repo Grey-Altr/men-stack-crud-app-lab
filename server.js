8558f61781ae5223bc5ac24a75752b43223a3ef6
GreyA <grey.altaer@gmail.com>
2025-03-04 15:16:59 -0800

    cleanup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
@@ -17,21 +18,7 @@ app.use(express.urlencoded.apply({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 
-// PUT /
-app.put('/instruments/:instrumentId', async (req, res) => {
-  if (req.body.owned === "on") {
-    req.body.owned = true;
-  } else {
-    req.body.owned = false;
-  };
-  await Instrument.findByIdAndUpdate(req.params.instrumentId, req.body);
-  res.redirect(`/instruments/${req.params.instrumentId}`);
-});
-
-
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
 // GET /
 app.get('/', async (req, res) => {
@@ -70,8 +57,23 @@ app.post('/instruments', async (req, res) => {
     res.redirect('/instruments');
 });
 
+// PUT /
+app.put('/instruments/:instrumentId', async (req, res) => {
+    if (req.body.owned === "on") {
+        req.body.owned = true;
+    } else {
+        req.body.owned = false;
+    };
+    await Instrument.findByIdAndUpdate(req.params.instrumentId, req.body);
+    res.redirect(`/instruments/${req.params.instrumentId}`);
+});
+
 // DELETE /
 app.delete('/instruments/:instrumentId', async (req, res) => {
     await Instrument.findByIdAndDelete(req.params.instrumentId);
     res.redirect('/instruments');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
 });
